33c1939168419b653fb1ff9338e9e294c03a9de5
TedKelvin <tedkelvin19@gmail.com>
2023-09-19 23:05:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun(activity="")
+
+function mondayWork(activity= "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+mondayWork(activity="")
+
+function wrapAdjective(result = "*") {
+    return function(emphatic) {
+      return `You are ${result}${emphatic}${result}!`;
+    }
+  }
+
+  const wrap = wrapAdjective();
+  console.log(wrap("hard worker"));
